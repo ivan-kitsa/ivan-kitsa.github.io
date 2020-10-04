f8fb3383c41d4c879448d90ac4f697aefc9f4a5f
ivan-kitsa <ivan@zenio.co>
2020-10-04 04:49:07 +0300

    TODO: Integration of move parts of sphere - 3 pointers for parts of sphere - Сonnect pointers to mouse - Up styles

---
+++ script.js
@@ -0,0 +1,13 @@
+// console.log(document.getElementsByTagName('attr'))
+
+// const  sphere = document.getElementById('sphere-shadow')
+
+
+const sphere = document.getElementById('sphere-body')
+const sphereShadow = document.getElementById('sphere-shadow')
+const sphereLight = document.getElementById('sphere-light')
+
+
+
+
+console.log(sphere)
